bd657c60c857fe7b7f2cd07c5db333bd1191ac5d
Wisdom Erhimwionsbo <20097898@mail.wit.ie>
2024-11-22 11:52:29 +0000

    Mongo lab Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -9,20 +10,28 @@ const app = express();
 
 const port = process.env.PORT;
 
+// Middleware to parse JSON
 app.use(express.json());
 
+// Define the error handler middleware
+const errHandler = (err, req, res, next) => {
+  /* If the error is in development then send the stack trace, 
+     if in production then just send the error message */
+  if (process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(
+    `Hey!! You caught the error 👍👍. Here's the details: ${err.stack}`
+  );
+};
+
+// Routes
 app.use('/api/tasks', tasksRouter);
+app.use('/api/users', usersRouter);
+
+// Use the error handler
 app.use(errHandler);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
-
-  const errHandler = (err, req, res, next) => {
-    /* if the error in development then send stack trace to display whole error,
-    if it's in production then just send error message  */
-    if(process.env.NODE_ENV === 'production') {
-      return res.status(500).send(`Something went wrong!`);
-    }
-    res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
-  };
 });
